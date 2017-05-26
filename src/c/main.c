9614e1c900199eb4a418ef7bcfb127557a942464
Kathleen Wang <kathleenxw@gmail.com>
2017-05-26 01:58:58 -0400

    Added tulip bitmaps
    Screen now shows a static image of a tulip in a clay pot with the time layered above the flower.

--- src/c/main.c
+++ src/c/main.c
@@ -5,21 +5,15 @@
 
 static Window *main_w;
 static TextLayer *time_layer;
-static Layer *canvas_layer;
-static GBitmap *pot;
-static BitmapLayer *pot_layer;
+static GBitmap *pot, *flower;
+static BitmapLayer *pot_layer, *flower_layer;
 
 /* ------------------------------------------------------------- */
 /* function declarations */
 
 //functions for handling updating the time shown on-screen
 static void minute_change_handler(struct tm *tick_time, TimeUnits units_changed);
-static void date_change_handler(struct tm *tick_time, TimeUnits units_changed);
 static void update_time();
-
-//functions for drawing on the canvas
-static void update_graphics();
-static void canvas_update_proc(Layer * layer, GContext * cntxt);
 
 //functions for properly initializing the main window
 static void main_w_load(Window *window);
@@ -52,7 +46,6 @@ static void init(){
     
     //attach time handlers
     tick_timer_service_subscribe(MINUTE_UNIT, minute_change_handler);
-    tick_timer_service_subscribe(DAY_UNIT, date_change_handler);
 }
 
 static void deinit(){
@@ -72,27 +65,29 @@ static void main_w_load(Window *window){
     time_layer = text_layer_create(GRect(0, PBL_IF_ROUND_ELSE(58,52), bounds.size.w, 50));
     text_layer_set_text(time_layer, "0:00");
     text_layer_set_text_alignment(time_layer, GTextAlignmentCenter);
-    
-    //format canvas layer
-    canvas_layer = layer_create(bounds);
-    layer_set_update_proc(canvas_layer, canvas_update_proc);
+    text_layer_set_background_color(time_layer, GColorFromRGBA(0, 0, 0, 0));    //set background to transparent
     
     //format bitmaps
-    pot = gbitmap_create_with_resource(RESOURCE_ID_CLAY_POT);
-    pot_layer = bitmap_layer_create(GRect(30,80, 95, 75));
+    //draw the flower
+    flower = gbitmap_create_with_resource(RESOURCE_ID_TULIP_4);
+    flower_layer = bitmap_layer_create(GRect(35, 20, 70, 85));
+    bitmap_layer_set_compositing_mode(flower_layer, GCompOpSet);
+    bitmap_layer_set_bitmap(flower_layer, flower);
+    //draw the pot
+    pot = gbitmap_create_with_resource(RESOURCE_ID_POT_CLAY);
+    pot_layer = bitmap_layer_create(GRect(47,105, 50, 48));
     bitmap_layer_set_compositing_mode(pot_layer, GCompOpSet);
     bitmap_layer_set_bitmap(pot_layer, pot);
     
     //add layers
-    layer_add_child(w_layer, text_layer_get_layer(time_layer));
-    layer_add_child(w_layer, canvas_layer);
+    layer_add_child(w_layer, bitmap_layer_get_layer(flower_layer));
     layer_add_child(w_layer, bitmap_layer_get_layer(pot_layer));
+    layer_add_child(w_layer, text_layer_get_layer(time_layer));
 }
 
 static void main_w_unload(Window *window){
     //destroy all
     text_layer_destroy(time_layer);
-    layer_destroy(canvas_layer);
     gbitmap_destroy(pot);
     bitmap_layer_destroy(pot_layer);
 }
@@ -104,10 +99,6 @@ static void minute_change_handler(struct tm *tick_time, TimeUnits units_changed)
     update_time();
 }
 
-static void date_change_handler(struct tm *tick_time, TimeUnits units_changed){
-    update_graphics();
-}
-
 static void update_time(){
     //get the current time
     time_t temp = time(NULL);
@@ -117,38 +108,4 @@ static void update_time(){
     static char s_buffer[8];
     strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ? "%H:%M" : "%I:%M", tick_time);    
     text_layer_set_text(time_layer, s_buffer);
-}
-
-/* ---------------------------------------------------------------------------------- */
-/* functions related to to the canvas layer */
-
-static void update_graphics(){
-    //update graphics layer
-    layer_mark_dirty(canvas_layer);
-}
-    
-static void canvas_update_proc(Layer * layer, GContext * cntxt){
-    //get information about the dimensions of the watch face
-//     GRect canvas_bounds = layer_get_bounds(layer);
-//     uint16_t middle_x = canvas_bounds.size.w / 2, //the x-coordinate for the middle of the watch
-//              middle_y = canvas_bounds.size.w / 2, //the y-coordinate
-//              radius = middle_x - 10;
-    
-    //format graphics
-    graphics_context_set_stroke_color(cntxt, GColorDarkCandyAppleRed);
-    graphics_context_set_fill_color(cntxt, GColorCeleste);
-    graphics_context_set_stroke_width(cntxt, 4);
-    graphics_context_set_antialiased(cntxt, true);
-    
-    //draw a circle
-//     GPoint center = GPoint(middle_x, middle_y);
-//     graphics_fill_circle(cntxt, center, radius);
-    
-    //draw the hour hand
-//     GPoint hour_start = GPoint(middle_x,middle_y), hour_end = GPoint(middle_x, middle_x + (radius / 2));
-//     graphics_draw_line(cntxt, hour_start, hour_end);
-    
-//     //draw the minute hand
-//     GPoint min_start = GPoint(middle_x,middle_y), min_end = GPoint(middle_x,middle_x + radius);
-//     graphics_draw_line(cntxt, min_start, min_end);   
 }
