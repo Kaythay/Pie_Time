f4f72d87398b8635cda27945b9b0574db0db54aa
Kathleen Wang <kathleenxw@gmail.com>
2017-05-30 00:58:05 -0400

    Fixed bug where display never updated
    Code needs to be refactored.

--- src/c/main.c
+++ src/c/main.c
@@ -4,6 +4,7 @@
 /* variable declarations */
 
 static Window *main_w;
+static Layer *canvas_layer;
 static TextLayer *time_layer;
 static GBitmap *pot, *flower;
 static BitmapLayer *pot_layer, *flower_layer;
@@ -11,11 +12,15 @@ static BitmapLayer *pot_layer, *flower_layer;
 /* ------------------------------------------------------------- */
 /* function declarations */
 
-//functions for handling updating screen
+//time change handlers
 static void minute_change_handler(struct tm *tick_time, TimeUnits units_changed);
-static void hour_change_handler(struct tm *tick_time, TimeUnits units_changed);
-static void update_time(struct tm *tick_time);
-static void update_flower(struct tm *tick_time);
+// static void hour_change_handler(struct tm *tick_time, TimeUnits units_changed);
+
+//functions for updating display
+static void update_time();
+static void update_flower();
+static uint32_t get_flower_resource(int time);
+static void update_canvas(Layer * layer, GContext *ctx);
 
 //functions for properly initializing the main window
 static void main_w_load(Window *window);
@@ -43,9 +48,13 @@ static void init(){
     
     window_stack_push(main_w, true);
     
+    //initialize the time and the flower
+    update_time();
+//     update_flower();
+    
     //attach time handlers
-    tick_timer_service_subscribe(MINUTE_UNIT, minute_change_handler);
-    tick_timer_service_subscribe(HOUR_UNIT, hour_change_handler);
+    tick_timer_service_subscribe(SECOND_UNIT, minute_change_handler);
+//     tick_timer_service_subscribe(SECOND_UNIT, hour_change_handler);
 }
 
 static void deinit(){
@@ -62,6 +71,7 @@ static void main_w_load(Window *window){
     GRect bounds = layer_get_bounds(w_layer);
     
     //create layers
+    canvas_layer = layer_create(bounds);
     time_layer = text_layer_create(GRect(0, PBL_IF_ROUND_ELSE(58,52), bounds.size.w, 50));
     flower_layer = bitmap_layer_create(GRect(35, 20, 70, 85));
     pot_layer = bitmap_layer_create(GRect(47,105, 50, 48));
@@ -73,20 +83,18 @@ static void main_w_load(Window *window){
     //format the flower layer
     bitmap_layer_set_compositing_mode(flower_layer, GCompOpSet);
     
-    //initialize the time and the flower
-    time_t temp = time(NULL);
-    struct tm *tick_time = localtime(&temp);
-    update_time(tick_time);
-    update_flower(tick_time);
-    
     //draw the pot
     pot = gbitmap_create_with_resource(RESOURCE_ID_POT_CLAY);
     bitmap_layer_set_compositing_mode(pot_layer, GCompOpSet);
     bitmap_layer_set_bitmap(pot_layer, pot);
     
+    //setup canvas procedure
+    layer_set_update_proc(canvas_layer, update_canvas);
+    
     //add layers
     layer_add_child(w_layer, bitmap_layer_get_layer(flower_layer));
     layer_add_child(w_layer, bitmap_layer_get_layer(pot_layer));
+    layer_add_child(w_layer, canvas_layer);
     layer_add_child(w_layer, text_layer_get_layer(time_layer));
 }
 
@@ -95,38 +103,75 @@ static void main_w_unload(Window *window){
     text_layer_destroy(time_layer);
     gbitmap_destroy(pot);
     bitmap_layer_destroy(pot_layer);
+    gbitmap_destroy(flower);
+    bitmap_layer_destroy(flower_layer);
+    layer_destroy(canvas_layer);
 }
 
 /* ---------------------------------------------------------------------------------- */
 /* time change handlers */
 
 static void minute_change_handler(struct tm *tick_time, TimeUnits units_changed){
-    update_time(tick_time);
+    update_time();
 }
 
-static void update_time(struct tm *tick_time){
+// static void hour_change_handler(struct tm *tick_time, TimeUnits units_changed){
+//     update_flower();
+// }
+
+/* ---------------------------------------------------------------------------------- */
+/* functions that update the display */
+
+static void update_time(){
+    //get the time
+    time_t temp = time(NULL);
+    struct tm *tick_time = localtime(&temp);
+    
     //update the text to show the current time
-    static char s_buffer[8];
-    strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ? "%H:%M" : "%I:%M", tick_time);    
+    static char s_buffer[10];
+    strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ? "%H:%M:%S" : "%I:%M:%S", tick_time);    
     text_layer_set_text(time_layer, s_buffer);
+    
+    update_flower();
 }
 
-static void hour_change_handler(struct tm *tick_time, TimeUnits units_changed){
-    update_flower(tick_time);
-}
-
-static void update_flower(struct tm *tick_time){
+static void update_flower(){
+    //get the time
+    time_t temp = time(NULL);
+    struct tm *tick_time = localtime(&temp);
+    
     //determine which flower to display
     //TODO: optimize?
-    int hour = tick_time->tm_hour;
-    if (hour >= 0 && hour < 6){
+    int hour = tick_time->tm_sec;
+    if (hour == 0){
         flower = gbitmap_create_with_resource(RESOURCE_ID_TULIP_1);
-    } else if (hour >= 6 && hour < 12){
+        bitmap_layer_set_bitmap(flower_layer, flower);
+    } else if (hour == 15){
         flower = gbitmap_create_with_resource(RESOURCE_ID_TULIP_2);
-    } else if (hour >= 12 && hour < 14){
+        bitmap_layer_set_bitmap(flower_layer, flower);
+    } else if (hour == 30){
         flower = gbitmap_create_with_resource(RESOURCE_ID_TULIP_3);
-    } else {
+        bitmap_layer_set_bitmap(flower_layer, flower);
+    } else if (hour == 45){
         flower = gbitmap_create_with_resource(RESOURCE_ID_TULIP_4);
+        bitmap_layer_set_bitmap(flower_layer, flower);
     }
-    bitmap_layer_set_bitmap(flower_layer, flower);
+}
+
+static uint32_t get_flower_resource(int time){
+    uint32_t resource_id;
+    if (time == 0){
+        resource_id = RESOURCE_ID_TULIP_1;
+    } else if (time == 15){
+        resource_id = RESOURCE_ID_TULIP_2;
+    } else if (time == 30){
+        resource_id = RESOURCE_ID_TULIP_1;
+    } else if (time == 45){
+        resource_id = RESOURCE_ID_TULIP_4;
+    }
+    return resource_id;
+}
+
+static void update_canvas(Layer * layer, GContext *ctx){
+    
 }
